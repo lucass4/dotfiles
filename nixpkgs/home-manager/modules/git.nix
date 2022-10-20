a0cc3e23d1f20ad16802489de3194db3ee1f7b11
Lucas Sant' Anna <76971778+lucass4@users.noreply.github.com>
2022-10-20 13:27:59 -0700

    adding git configuration

--- nixpkgs/home-manager/modules/git.nix
+++ nixpkgs/home-manager/modules/git.nix
@@ -1,2 +1,32 @@
-{ config, lib, pkgs, ... }:
-{}
+{ config, pkgs, lib, libs, ... }:
+{
+  programs.git = {
+    enable = true;
+    userName = "Lucas Sant' Anna";
+    userEmail = "76971778+lucass4@users.noreply.github.com";
+    signing.key = "56AE81F1E53DC9DC";
+    signing.signByDefault = true;
+   
+
+    delta = {
+      enable = true;
+      options = {
+        syntax-theme = "solarized-dark";
+        side-by-side = true;
+      };
+    };
+
+    extraConfig = {
+      pull.rebase = true;
+      init.defaultBranch = "main";
+      github.user = "lucass4";
+
+      push.autoSetupRemote = true;
+
+      core.editor = "nvim";
+      core.fileMode = false;
+      core.ignorecase = false;
+    };
+  };
+  
+}
