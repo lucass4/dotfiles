ad79e7d809ebf957f7d784c6ed252db81980fa04
Lucas Sant' Anna <76971778+lucass4@users.noreply.github.com>
2022-10-25 23:18:34 -0700

    wip

--- nixpkgs/home-manager/modules/config/nvim/lua/config/tree.lua
+++ nixpkgs/home-manager/modules/config/nvim/lua/config/tree.lua
@@ -5,21 +5,4 @@ vim.g.loaded_netrwPlugin = 1
 -- empty setup using defaults
 require("nvim-tree").setup()
 
--- OR setup with some options
-require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    adaptive_size = true,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-      },
-    },
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
-})
+vim.keymap.set('n', '<leader>e', ":NvimTreeOpen<CR>", {})
