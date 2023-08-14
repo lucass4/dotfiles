cc2c7aaebf867385d46e084bfee97c36d267ecad
Lucas Sant' Anna <76971778+lucass4@users.noreply.github.com>
2023-08-14 13:30:02 -0700

    fix: vim

--- modules/home-manager/modules/config/nvim/lua/config/keymaps.lua
+++ modules/home-manager/modules/config/nvim/lua/config/keymaps.lua
@@ -1,5 +1,4 @@
 -- local opts = { noremap = true, silent = true }
 --
--- vim.g.mapleader = " "
--- vim.g.maplocalleader = " "
-vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
