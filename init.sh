aa62a19e65f72b630be6b8ec3cd364c8d889d104
Lucas Sant' Anna <76971778+lucass4@users.noreply.github.com>
2025-08-31 10:34:18 -0700

    feat: replace init.sh with Makefile for setup and build

--- init.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-nix --extra-experimental-features "nix-command flakes" build ".#darwinConfigurations.$(hostname -s).system"
-
-./result/sw/bin/darwin-rebuild switch --flake .
