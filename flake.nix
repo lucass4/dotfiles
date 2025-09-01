a74067439a88113a934ac88a8b9058b96eb14585
Lucas Sant' Anna <76971778+lucass4@users.noreply.github.com>
2025-09-01 13:20:08 -0700

    chore(zellij): fix configuration and installing plugins

--- flake.nix
+++ flake.nix
@@ -2,71 +2,77 @@
   description = "Home Manager configuration of Lucas Santanna";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    nixpkgs.url = "github:NixOS/nixpkgs/master";
-
-    darwin.url = "github:lnl7/nix-darwin";
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
-
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
+    darwin = {
+      url = "github:lnl7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
     helix-themes = {
       url = "github:eureka-cpu/helix-themes.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     zjstatus = {
       url = "github:dj95/zjstatus";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, darwin, zjstatus, ... }: rec {
-    # Helper function to create Darwin configurations
-    createDarwinConfig = { name, system, username }: darwin.lib.darwinSystem {
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          (final: prev: {
-            zjstatus = zjstatus.packages.${prev.system}.default;
-          })
-        ];
+  outputs = inputs@{ self, nixpkgs, home-manager, darwin, ... }:
+    let
+      lib = nixpkgs.lib;
+
+      # Define hosts and their systems
+      hosts = {
+        "lucass-MacBook-Pro" = "x86_64-darwin";
+        "fg-lstanaanna" = "aarch64-darwin";
       };
-      specialArgs = { inherit inputs; }; # Pass inputs to modules
-      modules = [
+
+      # Common modules for all systems
+      commonModules = system: [
+        { nixpkgs.overlays = [ (final: prev: { zjstatus = inputs.zjstatus.packages.${system}.default; }) ]; }
         ./modules/darwin
         home-manager.darwinModules.home-manager
         {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            users."${username}".imports = [ ./modules/home-manager ];
-            extraSpecialArgs = { inherit inputs; }; # Pass inputs to home-manager modules
-          };
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.lucas.imports = [ ./modules/home-manager ];
+          home-manager.extraSpecialArgs = { inherit inputs; };
         }
       ];
-    };
 
-    darwinConfigurations = {
-      # Configuration matching the expected system name
-      "lucass-MacBook-Pro" = createDarwinConfig {
-        name = "lucass-MacBook-Pro";  # Matching the expected system name
-        system = "x86_64-darwin";      # Change to "aarch64-darwin" if needed
-        username = "lucas";
-      };
+      systems = [ "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      # Generate darwinConfigurations from hosts
+      darwinConfigurations = lib.mapAttrs (hostName: system:
+        darwin.lib.darwinSystem {
+          inherit system;
+          specialArgs = { inherit inputs; };
+          modules = commonModules system;
+        }
+      ) hosts;
 
-      # Example configuration for an ARM architecture, adjust as needed
-      "fg-lstanaanna" = createDarwinConfig {
-        name = "fg-lstanaanna";
-        system = "aarch64-darwin";
-        username = "lucas";
-      };
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              (final: prev: {
+                zjstatus = inputs.zjstatus.packages.${prev.system}.default;
+              })
+            ];
+          };
+        in
+        {
+          inherit pkgs;
+        }
+      );
     };
-  };
 }
 
